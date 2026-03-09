883e62418df12e34290f3dce3c9ec455776fe8b3
Khushal <khushal8002@gmail.com>
2026-03-09 13:55:23 +0530

    enable cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -555,3 +556,6 @@ app.get("/movies/:imdbID", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
+
+app.use(cors());
+app.use(express.json());
